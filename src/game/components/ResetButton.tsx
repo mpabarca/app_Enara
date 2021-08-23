32c44b10dfd342eba134e1784137a6036d734cc0
Paz <mariapaz.abarca@toctoc.com>
2021-08-22 23:14:43 -0400

    Create, delete and update structure of project

---
+++ src/game/components/ResetButton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResetButton = () => {
+    return <div>ResetButton</div>
+};
+
+export default ResetButton;
---
+++ src/game/components/WordBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WordBox = () => {
+    return <div>WordBox</div>
+};
+
+export default WordBox;
