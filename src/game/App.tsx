398233b285a5f26425e4da6a05256d6510fb7712
Paz <mariapaz.abarca@toctoc.com>
2021-08-23 19:52:32 -0400

    Add hooks to App component and its structure

--- src/game/App.tsx
+++ src/game/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [word, setWord] = useState(null);
+  
   return (
     <div className="App">
       <header className="App-header">
