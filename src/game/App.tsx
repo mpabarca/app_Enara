a4758b34c0139d2458363c2abeaa6f6f32bbf32e
Paz <mariapaz.abarca@toctoc.com>
2021-08-25 08:53:54 -0400

    Add dictionary validator to word

--- src/game/App.tsx
+++ src/game/App.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from "react-redux";
+import { RootState } from "./state/reducers";
 import boardJson from '../files/test-board-2.json';
+import dictionary from '../files/dictionary.json';
 import Board from './components/Board';
 import ResetButton from './components/ResetButton';
 import WordBox from './components/WordBox';
 
 const App: React.FC = () => {
+  const state = useSelector((state: RootState) => state.word);
+
   const [board, setBoard] = useState<string[]>([]);
   const [reloadBoard, setReloadBoard] = useState<boolean>(false);
+  const [validWord, setValidWord] = useState<boolean>(false);
 
   const getBoard = () => {
     return boardJson.board.sort(() => { return Math.random() - 0.5 });
@@ -23,12 +29,16 @@ const App: React.FC = () => {
     }
   }, [reloadBoard]);
 
+  useEffect(() => {
+    setValidWord(dictionary.words.includes(state));
+  },[state]);
+
   return (
     <div className="App d-flex flex-row flex-wrap justify-content-center align-items-center">
       <Board board = {board} /> 
-      <div className="d-flex flex-column justify-content-between info-board">
+      <div className="d-flex flex-column justify-content-between desktop info-board">
         <ResetButton setReloadBoard = {setReloadBoard}/>
-        <WordBox />
+        <WordBox validWord = {validWord} />
       </div>
     </div>
   );
--- src/game/components/WordBox.tsx
+++ src/game/components/WordBox.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../state/reducers";
 
-const WordBox = () => {
+type Props = {
+    validWord: boolean;
+};
+
+const WordBox: React.FC<Props> = ({ validWord }) => {
     const state = useSelector((state: RootState) => state.word);
+    console.log("en wordbox - " + validWord);
 
     return (
-        <div className="col-12 d-flex justify-content-start align-items-center word-box">
+        <div className="col-12 d-flex justify-content-between align-items-center word-box">
             {state}
+            <p>{validWord ? "valid" : "invalid"}</p>
         </div>
     )
 };
