e1ef91f00799dbd9f77ca2ded24b0e454c376cd1
Paz <mariapaz.abarca@toctoc.com>
2021-08-25 09:50:05 -0400

    Add validateWord to WordBox component

--- src/game/components/WordBox.tsx
+++ src/game/components/WordBox.tsx
@@ -1,19 +1,28 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useSelector, shallowEqual } from "react-redux";
 import { RootState } from "../state/reducers";
+import dictionary from '../../files/dictionary.json';
 
-type Props = {
-    validWord: boolean;
-};
+const WordBox = () => {
+    const [validWord, setValidWord] = useState<boolean>(false);
+    const [clickedWord, setClickedWord] = useState<boolean>(false);
 
-const WordBox: React.FC<Props> = ({ validWord }) => {
-    const state = useSelector((state: RootState) => state.word);
-    console.log("en wordbox - " + validWord);
+    const word = useSelector((state: RootState) => state.word, shallowEqual);
+
+    const validateWord = () => {
+        return dictionary.words.includes(word.toLowerCase());
+    };
+
+    useEffect(() => {
+        setValidWord(validateWord());
+        if(word !== "" && word !== undefined && word !== null) setClickedWord(true);
+        else setClickedWord(false);
+      },[word]);
 
     return (
         <div className="col-12 d-flex justify-content-between align-items-center word-box">
-            {state}
-            <p>{validWord ? "valid" : "invalid"}</p>
+            {validWord ? <h4 className="correct">{word}</h4> : <h4 className="wrong">{word}</h4>}
+            {clickedWord ? <p>{validWord ? <p className="correct">valid</p> : <p className="wrong">invalid</p>}</p> : ""}
         </div>
     )
 };
