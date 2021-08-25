14809a4a6dd4d4efdafbd2d17e30c83e1e8db6a2
Paz <mariapaz.abarca@toctoc.com>
2021-08-24 21:22:33 -0400

    Add dispatch resetWord to button in ResetButton component

--- src/game/components/ResetButton.tsx
+++ src/game/components/ResetButton.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../state";
 
 const ResetButton = () => {
+    const dispatch = useDispatch();
+    const { resetWord } = bindActionCreators(actionCreators, dispatch);
+
     return (
         <div>
             <p>Clear word</p>
-            <button type="button" className="btn-close" aria-label="Close" />
+            <button type="button" className="btn-close" aria-label="Close" onClick={() => resetWord()} />
         </div>
     )
 };
